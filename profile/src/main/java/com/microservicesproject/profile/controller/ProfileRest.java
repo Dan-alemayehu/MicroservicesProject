cb8de0763a32d5ad5d28e9a2855ff6a570d67b4e
Daniel Alemayehu <alemayehu.2810@gmail.com>
2025-01-14 12:08:19 -0600

    Third commit, started circuit breaker.

--- profile/src/main/java/com/microservicesproject/profile/controller/ProfileRest.java
+++ profile/src/main/java/com/microservicesproject/profile/controller/ProfileRest.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/profiles")
+@RequestMapping("/api/profiles")
 @Slf4j
 @RequiredArgsConstructor
 public class ProfileRest {
